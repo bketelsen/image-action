f74c91da29f04f0a5dfce72af8de7493d467f012
Brian Ketelsen <bketelsen@gmail.com>
2021-05-21 07:04:29 -0400

    try entrypoint

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
 const { processImages } = require('./images');
 
 
 async function run() {
-  const src = core.getInput('source-directory');
-  const target = core.getInput('output-directory');
+  const src = process.argv[2]
+  const target = process.argv[3]
   await processImages(src,target);
 }
 
 try {
     run();
 } catch (error) {
-  core.setFailed(error.message);
+  console.log(error.message);
 }
